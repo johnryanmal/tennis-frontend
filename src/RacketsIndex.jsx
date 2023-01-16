f7c0af394b83b89f88944615cc7ff3334c472458
johnryanmal <johnryanmal@gmail.com>
2023-01-16 12:23:34 -0800

    make rackets index dynamic

--- src/RacketsIndex.jsx
+++ src/RacketsIndex.jsx
@@ -4,20 +4,34 @@ import { useEffect, useState } from 'react';
 export function RacketsIndex() {
 	const [ rackets, setRackets ] = useState([1,2,3])	
 
-	useEffect(() => {
+	const getRackets = () => {
 		axios.get('http://localhost:3000/rackets')
 		.then((response) => {
-			console.log(response)
+			let data = response.data?.rackets
+			console.log(data)
+			if (data) {
+				setRackets(data)
+			}
 		})
-	})
+		.catch((error) => {
+			console.error(error)
+		})
+	}
 
+	useEffect(getRackets, [])
+
+	const serializeRacket = (racket) => (
+		<div>
+			<h2>Racket</h2>
+			<p>Created at: {racket.created_at}</p>
+			<p>Updated at: {racket.updated_at}</p>
+		</div>
+	)
 
 	return (
 		<div>
 			<h1>Rackets Index</h1>
-			{rackets.map((racket) => (
-				<p>{racket}</p>
-			))}
+			{rackets.map(serializeRacket)}
 		</div>
 	)
 }
