7fcc2d6b449079815e227c0e3b02e7b248738f6b
johnryanmal <johnryanmal@gmail.com>
2023-01-17 09:47:02 -0800

    axios racket new

--- src/RacketsNew.jsx
+++ src/RacketsNew.jsx
@@ -3,6 +3,14 @@ import axios from 'axios'
 export function RacketsNew() {
 	const handleSubmit = (event) => {
 		event.preventDefault()
+		let params = new FormData(event.target)
+		axios.post('http://localhost:3000/rackets', params)
+		.then((response) => {
+			console.log(response)
+		})
+		.catch((error) => {
+			console.error(error)
+		})
 	}
 
 	return (
