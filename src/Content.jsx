282688b979898cbf895c9daf15e0f45cbeab7bd0
johnryanmal <johnryanmal@gmail.com>
2023-01-17 10:25:04 -0800

    refactor rackets index

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,31 @@
 import { RacketsIndex }from './RacketsIndex'
-import { RacketsNew }from './RacketsNew'
+import { RacketsNew } from './RacketsNew'
+import axios from 'axios'
+import { useState, useEffect  } from 'react'
 
 export function Content() {
+  const [rackets, setRackets] = useState([])
+
+  const getRackets = () => {
+		axios.get('http://localhost:3000/rackets')
+		.then((response) => {
+			let data = response.data?.rackets
+			console.log(data)
+			if (data) {
+				setRackets(data)
+			}
+		})
+		.catch((error) => {
+			console.error(error)
+		})
+	}
+
+	useEffect(getRackets, [])
+
   return (
     <div>
       <RacketsNew />
-      <RacketsIndex />
+      <RacketsIndex rackets={rackets}/>
     </div>
   );
 }
--- src/RacketsIndex.jsx
+++ src/RacketsIndex.jsx
@@ -1,30 +1,10 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import { RacketsShow } from './RacketsShow';
 
-export function RacketsIndex() {
-	const [ rackets, setRackets ] = useState([1,2,3])	
-
-	const getRackets = () => {
-		axios.get('http://localhost:3000/rackets')
-		.then((response) => {
-			let data = response.data?.rackets
-			console.log(data)
-			if (data) {
-				setRackets(data)
-			}
-		})
-		.catch((error) => {
-			console.error(error)
-		})
-	}
-
-	useEffect(getRackets, [])
-
+export function RacketsIndex(props) {
 	return (
 		<div>
 			<h1>Rackets Index</h1>
-			{rackets.map((racket, index) => (
+			{props.rackets?.map((racket, index) => (
 				<RacketsShow key={index} racket={racket}/>
 			))}
 		</div>
